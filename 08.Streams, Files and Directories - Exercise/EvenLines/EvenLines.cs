32223609bfc5fd281181c0ad3dd689a28f44609e
Bojidar Dermendjiev <bojidardermendjiew@abv.bg>
2023-03-24 16:39:44 +0200

    Upload solutions for 08.Streams, Files and Directories - Exercise

---
+++ 08.Streams, Files and Directories - Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,41 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+        public static string ProcessLines(string inputFilePath)
+        {
+            string output = string.Empty;
+            var charsToReplace = new[] { '-', ',', '.', '!', '?' };
+            string currentLine;
+            int counter = default;
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                while ((currentLine = reader.ReadLine()) != null)
+                {
+                    counter++;
+                    if (counter % 2 == 0)
+                        continue;
+                    StringBuilder sb = new StringBuilder(currentLine);
+                    for (int currentSymbol = 0; currentSymbol < sb.Length; currentSymbol++)
+                        if (charsToReplace.Contains(sb[currentSymbol]))
+                            sb[currentSymbol] = '@';
+                    var words = new List<string>(sb.ToString().Split());
+                    words.Reverse();
+                    output += String.Join(" ", words) + Environment.NewLine;
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ 08.Streams, Files and Directories - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,40 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            var output = new StringBuilder();
+            int lineCounter = 1;
+            string line;
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    MatchCollection letterMatches = Regex.Matches(line, @"[A-Za-z]");
+                    MatchCollection punctuationMatches = Regex.Matches(line, @"[-.!,?']");
+                    output.Append($"Line {lineCounter}: ")
+                        .Append(line)
+                        .Append($" ({letterMatches.Count})({punctuationMatches.Count})")
+                        .AppendLine();
+                    lineCounter++;
+                }
+            }
+            using (var writer = new StreamWriter(outputFilePath))
+                writer.Write(output);
+        }
+    }
+}
